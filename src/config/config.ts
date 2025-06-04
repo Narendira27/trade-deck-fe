d75db4cd967c274b9706c7c60fd7de04666b58cd
Narendira <narendira2701@gmail.com>
2025-06-04 23:54:10 +0530

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:54321";
+// export const API_URL = "http://localhost:54321";
 
-// export const API_URL = "https://aruba-tee-jamie-reno.trycloudflare.com";
+export const API_URL = "https://enjoyed-strap-tag-faq.trycloudflare.com";
