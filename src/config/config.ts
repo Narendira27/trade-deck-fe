5fa7e9808e8bf451e3ec4d4c284f2bd9a7f89f45
insanely-dev <narendira87@outlook.com>
2025-06-28 23:57:28 +0530

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,1 @@
-// export const API_URL = "https://api.tradedeck.narendira.in";
-
-// export const SOCKET_MAIN = "https://mainsocket.tradedeck.narendira.in/";
-
-// export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 
