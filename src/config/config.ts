d1217d63af01b1805b15c8bc7d9971b4b4bad012
insanely-dev <narendira87@outlook.com>
2025-06-28 23:52:16 +0530

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,3 @@
 
 // export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 
-export const API_URL = "http://localhost:3000";
-
-export const SOCKET_MAIN = "http://localhost:3001";
-
-export const SOCKET_FE = "http://localhost:3002";
