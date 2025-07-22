ec3fa28745e8644d69492f086f165f1838752c9a
Narendira27 <41791017+Narendira27@users.noreply.github.com>
2025-07-22 18:07:09 +0530

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,10 @@
 // export const SOCKET_MAIN = "https://mainsocket.tradedeck.narendira.in";
 // export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 
-export const API_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:3000";
 export const SOCKET_MAIN = "http://localhost:3001";
 export const SOCKET_FE = "http://localhost:3002";
 
-// export const API_URL = "https://walk-girlfriend-marvel-backup.trycloudflare.com";
+export const API_URL = "https://persons-elliott-alien-gabriel.trycloudflare.com/";
 // export const SOCKET_MAIN = "https://barrel-custom-swap-broke.trycloudflare.com";
 // export const SOCKET_FE = "https://upc-rotary-conditioning-blah.trycloudflare.com";
