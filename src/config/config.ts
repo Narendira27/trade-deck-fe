6a86f3b38dd8c525ee2d4f1a6a53a8d176b746a5
narendira01 <156963066+narendira01@users.noreply.github.com>
2025-08-06 00:04:29 +0530

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,11 @@
 // export const SOCKET_MAIN = "https://mainsocket.tradedeck.narendira.in";
 // export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 
-export const API_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:3000";
 export const SOCKET_MAIN = "http://localhost:3001";
 export const SOCKET_FE = "http://localhost:3002";
 export const SOCKET_INFO = "http://localhost:3003";
 
-// export const API_URL = "https://endless-locks-kill-theater.trycloudflare.com";
+export const API_URL = "https://mic-antarctica-preparing-navigation.trycloudflare.com";
 // export const SOCKET_MAIN = "https://barrel-custom-swap-broke.trycloudflare.com";
 // export const SOCKET_FE = "https://upc-rotary-conditioning-blah.trycloudflare.com";
