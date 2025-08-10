f1e932216b6bc58a916e480e198c565f1d8db7fc
Narendira27 <41791017+Narendira27@users.noreply.github.com>
2025-08-10 18:08:45 +0530

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,7 +9,7 @@ export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 // export const SOCKET_INFO = "http://localhost:3003";
 
 export const API_URL =
-"https://network-younger-two-wool.trycloudflare.com";
+"https://lectures-collaboration-where-howard.trycloudflare.com";
 export const SOCKET_INFO = "https://rotation-command-pose-please.trycloudflare.com";
 // export const SOCKET_MAIN = "https://barrel-custom-swap-broke.trycloudflare.com";
 // export const SOCKET_FE = "https://upc-rotary-conditioning-blah.trycloudflare.com";
