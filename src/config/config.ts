f79b385542bb3f694199e0178c93b9a8f3b998b2
Narendira <narendira2701@gmail.com>
2025-10-01 14:13:36 +0530

    fix : issues

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
-// export const API_URL = "https://api.tradedeck.narendira.in";
+export const API_URL = "https://api.tradedeck.narendira.in";
 export const SOCKET_MAIN = "https://mainsocket.tradedeck.narendira.in";
 export const SOCKET_FE = "https://fesocket.tradedeck.narendira.in";
 export const SOCKET_INFO = "https://wssfrontend.tradedeck.narendira.in";
 
-export const API_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:3000";
 // export const SOCKET_MAIN = "http://localhost:3001";
 // export const SOCKET_FE = "http://localhost:3002";
 // export const SOCKET_INFO = "http://localhost:3003";
